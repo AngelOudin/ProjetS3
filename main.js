8544f05850caf329814b6dd5156a6f3cf6ab9177
DGhidalia <david.ghidalia@outlook.fr>
2016-11-11 15:03:24 +0100

    ptitemaison
    loading ptitemaison

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ var control,camera,scene,renderer,width,height,cubedae,degree;
 
 	var collada=new THREE.ColladaLoader();
 	collada.options.convertUpAxis=true;
-	collada.load("assets/cubedae.dae",function(object){
+	collada.load("assets/ptitemaison.dae",function(object){
 		cubedae=object.scene;
 		cubedae.scale.set(30,30,30);
 		cubedae.position.set(0,80,-56);
-		cubedae.rotation.set(0,180 * degree,0);
+		//cubedae.rotation.set(0,180 * degree,0);
 
 	init();
 	animate();
@@ -26,8 +26,13 @@ var control,camera,scene,renderer,width,height,cubedae,degree;
 		var light=new THREE.PointLight(0xffffff,1,2000);
 		light.position.set(500,500,500);
 
+		var light2 = new THREE.PointLight(0xffffff,1,2000);
+		light2.position.set(-500,-500,-500);
+
 		scene.add(light);
+		scene.add(light2);
 		scene.add(cubedae);
+		cubedae.position.set(25,56,-56);
 
 		renderer=new THREE.WebGLRenderer();
 		renderer.setClearColor(0xffffff);
